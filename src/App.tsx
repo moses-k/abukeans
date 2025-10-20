3a9ca15d39e1ec5bb3e00660264790816196bd75
moses-k <moseskipyegonm@gmial.com>
2025-10-20 17:34:10 +0300

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Testimonials from './components/Testimonials';
 import Team from './components/Team';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <div className="App">
